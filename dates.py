b384768465551f4a139ffd732060377811ac6e5f
Gabriel Freedman <gfreedman2108@comcast.net>
2019-08-25 23:09:23 -0500

    Create date functions
    Wrote functions to generate list of dates for the previous week going back to last Monday.

---
+++ dates.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from datetime import date, timedelta
+
+
+def get_month(today):
+    month = today.strftime('%B').lower()
+    return month
+
+
+def get_previous_month(today):
+    first_of_month = today.replace(day=1)
+    prev = first_of_month - timedelta(days=1)
+    prev_month = prev.strftime('%B').lower()
+    return prev_month
+
+
+def get_recent_monday(today):
+    days_ahead = today.weekday()
+    if today.weekday() == 0:
+        recent_monday = today
+    elif today.weekday() <= 4:
+        recent_monday = today + timedelta(days=days_ahead)
+    else:
+        days_behind = 7 - days_ahead
+        recent_monday = today + timedelta(days=days_behind)
+
+    return recent_monday
+
+
+def get_last_week_dates(today):
+    week_end = get_recent_monday(today)
+    week_start = week_end - timedelta(days=7)
+    week_dates = [week_start + timedelta(days=i) for i in range(7)]
+    week_dates_fmt = [day.strftime('%#m/%#d') for day in week_dates]
+    return week_dates_fmt
---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from datetime import date
+
+import requests
+from bs4 import BeautifulSoup
+
+today = date.today()
+
